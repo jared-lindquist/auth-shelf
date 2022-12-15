a6827ce86e8d72783afd1def45740730bcd70f37
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-15 13:56:40 -0600

    edit functioning on client side

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -3,20 +3,37 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import useReduxStore from '../../hooks/useReduxStore';
 import AddItem from '../AddItem/AddItem';
+import { useState } from 'react';
+import EditItem from '../EditItem/EditItem';
 
 function ShelfPage() {
+
+	// let editState = false;
+	const [editState, setEditState] = useState(false);
 	const store = useReduxStore();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
+		
 		dispatch({ type: 'FETCH_ITEMS' });
 	}, []);
+
+
+	const handleEdit = (item) => {
+		setEditState(true);
+		
+		console.log('in handleEdit', item, editState);
+
+		dispatch({type: 'EDIT_ITEM', payload: item})
+
+	}
+
 	
 	//TODO Update the (store.map) to match the reducer that will be created later on.
 	return (
 		<div className='container'>
 			{/*{JSON.stringify({ store })} */}
-			<AddItem />
+			{editState ? <EditItem /> : <AddItem />}
 			<h2>Shelf</h2>
 			<p>All of the available items can be seen here.</p>
 			<table>
@@ -30,6 +47,7 @@ function ShelfPage() {
 							<td>{item.description}</td>
 							<td><img src={item.image_url} /></td>
 							<td><button onClick={() => dispatch({type: 'DELETE_ITEM', payload: item})}>Delete Me</button></td>
+							<td><button onClick={() => handleEdit(item)}>Edit item</button></td>
 						</tr>
 					);
 				})}
